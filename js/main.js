c3cbb9a6466ae0aff136eb27c216452ab5b39052
mark.barretto <mark.barretto@gmail.com>
2014-06-20 19:05:18 +0800

    edited der

--- js/main.js
+++ js/main.js
@@ -97,20 +97,16 @@ app.controller('UploadController', ['$scope', 'Restangular', '$routeParams',
 						contentType: false,
 						success: function(data){
 //						jQuery('#uploadedImage').append('<img src="'+data['details']['content-url']+data['details']['content-name']+'"/>');
+						$scope.$apply(function() {
 						$scope.formData.name = data['details']['content-name'];
-						$scope.formData.url = data['details']['content-url'];						
+						$scope.formData.url = data['details']['content-url'];
+						});						
 						},
 				   
 					});
 				}
 			})
 		}
-	
-		$scope.changeEntries = function () {
-			$scope.formData.name="benjo";
-			alert("benjo");
-		};
-
         
         $scope.processForm = function () {
             $scope.formData.tags.push($scope.formData.section);
