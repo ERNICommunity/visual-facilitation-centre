2f8080e4d46b94ab4ba2c95c3d8d1ba860481265
mark.barretto <mark.barretto@gmail.com>
2014-07-05 23:29:12 +0800

    updated updater

--- update.php
+++ update.php
@@ -6,11 +6,6 @@ if($headers['X-Hub-Signature'] == '1B788A18E'){
 	if(`git pull origin master`){
 		http_response_code('200');
 	}
-
-	if(isset($_POST[])){ 
-		echo json_encode($_POST[], JSON_UNESCAPED_SLASHES);
-		
-	};
 }
 
 ?>
