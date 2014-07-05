493d8f4639163c3c313d80e7a9eac875bf332eb9
mark.barretto <mark.barretto@gmail.com>
2014-07-05 22:52:08 +0800

    removed security

--- update.php
+++ update.php
@@ -1,10 +1,10 @@
 <?php 
-$headers = http_get_request_headers();
+//$headers = http_get_request_headers();
 
-if($headers['X-Hub-Signature'] == '1B788A18E'){
+//if($headers['X-Hub-Signature'] == '1B788A18E'){
 	if($_POST[]){ 
 		echo `git pull origin master`;
 	};
-}
+//}
 
 ?>
