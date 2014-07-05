e4e289a1cf8cecb0ed6fb699ed6d41c8e7d57e39
mark.barretto <mark.barretto@gmail.com>
2014-07-05 23:38:12 +0800

    updated updater

--- update.php
+++ update.php
@@ -1,11 +1,11 @@
 <?php 
-$headers = http_get_request_headers();
+//$headers = http_get_request_headers();
 
-if($headers['X-Hub-Signature'] == '1B788A18E'){
+//if($headers['X-Hub-Signature'] == '1B788A18E'){
 	echo `whoami`;
 	echo `git pull origin master`;
 	http_response_code('200');
 	
-}
+//}
 
 ?>
