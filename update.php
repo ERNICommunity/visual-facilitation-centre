df424b83a3ded9e3f6794ec9c43e4c9493bacc70
mark.barretto <mark.barretto@gmail.com>
2014-07-05 23:11:10 +0800

    updated updater to return json response

--- update.php
+++ update.php
@@ -2,8 +2,10 @@
 $headers = http_get_request_headers();
 
 if($headers['X-Hub-Signature'] == '1B788A18E'){
+	
+	
 	if($_POST[]){ 
-		
+		echo json_encode($_POST[], JSON_UNESCAPED_SLASHES);
 		if(`git pull origin master`){
 			http_response_code('200');
 		}
