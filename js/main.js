ec449d7de650f1e0188c484dc7b54fd159843c94
mark.barretto <mark.barretto@gmail.com>
2014-08-29 18:41:14 +0800

    added routes redirect

--- js/main.js
+++ js/main.js
@@ -52,7 +52,20 @@
 
                     redirectTo: 'content/basics'
                 });
-        }]);
+        }]).run( function($rootScope, $location) {
+			// register listener to watch route changes
+			$rootScope.$on( "$routeChangeStart", function(event, next, current) {
+				if ( $rootScope.loggedUser == null ) {
+					// no logged user, we should be going to #login
+					if ( next.templateUrl == "Sections/login.html" ) {
+						// already going to #login, no redirect needed
+					} else {
+						// not going to #login, we should redirect now
+						$location.path( "/login" );
+					}
+				}         
+			});
+ 		});
 
     app.controller('LoginController', ['$scope', 'Restangular', '$routeParams', '$http', '$cookies',
         function LoginCtrl($scope, db, $routeParams, $http, $cookies) {
