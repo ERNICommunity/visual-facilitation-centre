f68df449fee70ac0d158e4269b444ad4a410bdf9
mark.barretto <mark.barretto@gmail.com>
2014-09-10 21:25:41 +0800

    move show user to service

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@
         	}
         }]);
 
-    app.controller('LoginController', ['Global','$scope', '$rootScope', 'Restangular', '$routeParams', '$http', '$cookies',
+    app.controller('LoginController', ['Global', '$scope', '$rootScope', 'Restangular', '$routeParams', '$http', '$cookies',
         function LoginCtrl($scope, $rootScope, db, $routeParams, $http, $cookies) {
 
             $scope.setUserProfileInViewsModel = function () {
