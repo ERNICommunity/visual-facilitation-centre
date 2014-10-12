42e24f5e5333b9339b2e48585c0e9f8cd8602b47
mark.barretto <mark.barretto@gmail.com>
2014-10-12 14:34:11 +0800

    updated delete

--- delete_ajax.php
+++ delete_ajax.php
@@ -6,17 +6,17 @@ $output = [];
 $status = 404;
 $message = 'Not Found';
 
-if(isset($_POST)){
-	if(isset($_POST['delete']['name'])){
-			if(file_exists($path . $_POST['delete']["name"])){
-				unlink($path . $_POST['delete']["name"]);
-				$status = 201;
-				$message = 'deleting file '.$_POST['delete']["name"].' successful';
-			} else {
-				$status = 204;
-				$message = "Error: " . $_POST['delete']["name"] . 'not found';
-			}
+if(isset($_REQUEST)){
+	if(isset($_REQUEST['name'])){
+		if(file_exists($path . $_REQUEST["name"])){
+			unlink($path . $_REQUEST["name"]);
+			$status = 201;
+			$message = 'deleting file '.$_REQUEST["name"].' successful';
+		} else {
+			$status = 204;
+			$message = "Error: " . $_REQUEST["name"] . 'not found';
 		}
+		
 	}  else {
 		$status = 209;
 		$message = "specified file for deletion not found";
