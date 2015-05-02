042c8c38963487bbf7665b78bc9bbe71a8f36900
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 12:40:37 +0200

    implementing file resizing.

--- upload.php
+++ upload.php
@@ -1,11 +1,19 @@
 <?php
 
+
+
+
+
 if ( !empty( $_FILES ) ) {
 
     $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
     $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
 
     move_uploaded_file( $tempPath, $uploadPath );
+
+    $output = shell_exec('convert {$uploadPath} -resize 400 -quality 90 {$uploadPath}');
+
+
 
     $answer = array( 'answer' => 'File transfer completed' );
     $json = json_encode( $answer );
