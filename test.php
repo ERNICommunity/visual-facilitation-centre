b0412c793cc669c16281c5e3179983165a46ed89
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 12:05:32 +0200

    playing around with resizing pictures

--- test.php
+++ test.php
@@ -1,7 +1,10 @@
 <?php
 
       echo '<p>Hello World</p>' ;
-$output = shell_exec('ls -lart');
+
+
+
+$output = shell_exec('convert testcase.png -resize 400 -quality 90 testcase2.png');
 
 
           echo $output   ;
