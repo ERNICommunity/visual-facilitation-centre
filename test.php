f2259fd3fa217ab271e5499254b609d2f147214c
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 12:05:54 +0200

    playing around with resizing pictures

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 
 
 
-$output = shell_exec('convert testcase.png -resize 400 -quality 90 testcase2.png');
+$output = shell_exec('convert testcase.png -resize 400 -quality 90 testcase3.png');
 
 
           echo $output   ;
