aa07f520f14fe8b36b42a946aec5678028584987
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 12:27:17 +0200

    shell command test

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
       echo '<p>Hello World</p>';
-$output = shell_exec('ls');
+$output = shell_exec('convert uploads/testcase.png -resize 400 -quality 90 uploads/testcase6.png');
 
 
           echo $output;
