2e38bd160e41b23bbf597e276707e8c02b0b4775
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 14:16:41 +0200

    remove legacy code

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
       echo '<p>Hello World</p>';
-$output = shell_exec('convert uploads/testcase.png -resize 400 -quality 90 uploads/testcase6.png');
+$output = shell_exec("convert '/srv/www/github.visualfacilitation/uploads/8trepj6f9_2012-04-23 12.27.03.jpg' -resize 400 -quality 90 '/srv/www/github.visualfacilitation/uploads/8trepj6f9_2012-04-23 12.27.03.jpg'");
 
 
           echo $output;
--- upload.php
+++ upload.php
@@ -3,33 +3,26 @@
 
 if ( !empty( $_FILES ) ) {
 
-
-error_log("your message");
-
-
-$name = $_FILES["file"]["name"];
-
     $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
-
-
-    error_log($tempPath);
+    $ext = pathinfo($tempPath, PATHINFO_EXTENSION);
 
     $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
 
 
-
-    error_log('uploads : ' . $uploadPath);
+    $cmd = "convert '" . $uploadPath . "' -resize 400 -quality 90 '" . $uploadPath . "'";
 
 
     move_uploaded_file( $tempPath, $uploadPath );
 
 
+
+
+    $output = shell_exec($cmd);
+
     $answer = array( 'answer' => 'File transfer completed' );
     $json = json_encode( $answer );
 
     echo $json;
-
-    error_log("END");
 
 } else {
 
