5c86626d4d29d149bee190129db4a42a8357cbb7
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 13:52:24 +0200

    remove legacy code

--- upload.php
+++ upload.php
@@ -3,13 +3,20 @@
 
 if ( !empty( $_FILES ) ) {
 
+
+$name = $_FILES["file"]["name"];
+$ext = end((explode(".", $name))); # extra () to prevent notice
+
+
+
+
     $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
     $ext = pathinfo($tempPath, PATHINFO_EXTENSION);
 
     $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
 
 
-    $cmd = 'convert ' . $tempPath . ' -resize 400 -quality 90 ' . $tempPath;
+    $cmd = 'convert ' . $tempPath . '.' . $ext . ' -resize 400 -quality 90 ' . $tempPath. '.' . $ext;
 
 
     move_uploaded_file( $tempPath, $uploadPath );
