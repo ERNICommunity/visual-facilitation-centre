e616b104d14ea7567910d36f553b0d7f2e74e26e
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-02 13:16:06 +0200

    remove legacy code

--- upload.php
+++ upload.php
@@ -1,14 +1,5 @@
 <?php
 
-         function resize(input){
-         	$thumb = new Imagick(input);
-         	$thumb->resizeImage(400,400,Imagick::FILTER_LANCZOS,1);
-         	$thumb->writeImage(input);
-         	$thumb->clear();
-         	$thumb->destroy();
-         }
-
-
 
 if ( !empty( $_FILES ) ) {
 
@@ -21,7 +12,10 @@ if ( !empty( $_FILES ) ) {
     move_uploaded_file( $tempPath, $uploadPath );
 
 
-    $output = shell_exec('convert uploads/' . $_FILES[ 'file' ][ 'name' ] . '-resize 400 -quality 90 uploads/' . $_FILES[ 'file' ][ 'name' ]);
+    $cmd = 'convert uploads/' . $_FILES[ 'file' ][ 'name' ] . '-resize 400 -quality 90 uploads/' . $_FILES[ 'file' ][ 'name' ];
+
+
+    $output = shell_exec($cmd);
 
     $answer = array( 'answer' => 'File transfer completed' );
     $json = json_encode( $answer );
