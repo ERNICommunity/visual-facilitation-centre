013c87740f704f1fb7b6ec05126836ba7230fb5b
drjazz@hotmail.com <drjazz@hotmail.com>
2015-05-12 00:19:16 +0200

    add access methods

--- js/main.js
+++ js/main.js
@@ -100,18 +100,6 @@
     });
 
 
-    app.config(['$httpProvider',
-        function ($httpProvider) {
-            // Use x-www-form-urlencoded Content-Type
-            $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-
-            // Override $http service's default transformRequest
-            $httpProvider.defaults.transformRequest = [function (data) {
-                return angular.isObject(data) && String(data) !== '[object File]' ? $.param(data) : data;
-            }];
-        }]);
-
-
     app.config(['$routeProvider', 'ngDialogProvider',
             function ($routeProvider, ngDialogProvider) {
                 $routeProvider.
